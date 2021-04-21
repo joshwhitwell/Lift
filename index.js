0842a3680438ed24a04b6b8b1fc81af32156c188
Josh Whitwell <josh.whitwell@gmail.com>
2021-04-21 14:56:04 -0400

    adds eslint, prettier, and husky

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require("express")
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const server = express()
-server.use(express.json())
-server.use(express.static(path.join(__dirname, 'client/build')))
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const server = express();
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "client/build")));
 
-server.get('/api', (req, res) => {
-    res.json({ message: 'API is up!!!' })
-})
+server.get("/api", (req, res) => {
+  res.json({ message: "API is up!!!" });
+});
 
-server.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'client/build', 'index.html')) })
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 server.listen(PORT, () => {
-    console.log(`\n*** Server listening on PORT:${PORT} ***\n`)
-})
+  console.log(`\n*** Server listening on PORT:${PORT} ***\n`);
+});
