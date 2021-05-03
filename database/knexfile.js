fe3dcec5e20b438e4e051222c713c96cfdd79290
Josh Whitwell <josh.whitwell@gmail.com>
2021-05-03 17:20:04 -0400

    adds postgres connection string

--- database/knexfile.js
+++ database/knexfile.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.PG_CONNECTION_STRING,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./migrations",
     },
