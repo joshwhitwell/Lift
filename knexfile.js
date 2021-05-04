e07ee218ddc7e7df08a08e5476852dddb290fcb3
Josh Whitwell <josh.whitwell@gmail.com>
2021-05-04 12:18:16 -0400

    moves knexfile to root directory

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./development.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+};
