84b6cfd88fd265653f8443d916ea13fcc0a332b5
Josh Whitwell <josh.whitwell@gmail.com>
2021-05-07 13:26:12 -0400

    adds error logs and removes debugging logs

--- api/routes/users/userModel.js
+++ api/routes/users/userModel.js
@@ -3,14 +3,17 @@ const db = require("../../../database/databaseConfig");
 
 //access methods
 function find() {
-  return db("users");
+  try {
+    return db("users");
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 }
 
 async function add(user) {
-  console.log("add user hit");
   try {
     const result = await db("users").insert(user);
-    console.log.log("add user result", result);
     return result;
   } catch (err) {
     console.log(err);
--- api/routes/users/userRouter.js
+++ api/routes/users/userRouter.js
@@ -9,19 +9,19 @@ router.get("/", async (_req, res, next) => {
     const users = await Users.find();
     res.status(200).json({ users });
   } catch (err) {
+    console.log(err);
     next(err);
   }
 });
 
 //[POST] /
 router.post("/", async (req, res, next) => {
-  console.log("post user / hit");
   const { uid, name } = req.decoded;
   try {
     const result = await Users.add({ user_id: uid, user_name: name });
-    console.log("post user / result", result);
-    res.status(200).json(result);
+    res.status(200).json({ result });
   } catch (err) {
+    console.log(err);
     next(err);
   }
 });
